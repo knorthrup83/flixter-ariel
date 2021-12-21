a35d44d17b6f2e30055d1ad9a8c68045bc9eb179
Ariel Abelein <arielabelein@gmail.com>
2021-12-21 19:34:33 +0000

    finishing touches

--- app/controllers/instructor/sections_controller.rb
+++ app/controllers/instructor/sections_controller.rb
@@ -3,9 +3,6 @@ before_action :authenticate_user!
 before_action :require_authorized_for_current_course, only: [:create]
 before_action :require_authorized_for_current_section, only: [:update]
 
-    def new
-        @section = Section.new
-    end
 
     def create
         @section = current_course.sections.create(section_params)
