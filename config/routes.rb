fc82a9e0fe3ffecdca14498d6c4f5d82b9f160e2
Ariel Abelein <arielabelein@gmail.com>
2021-12-22 17:03:04 +0000

    finished

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,9 @@ Rails.application.routes.draw do
   devise_for :users
   resource :dashboard, only: [:show]
   root 'static_pages#index'
+  get 'privacy', to: 'static_pages#privacy'
+  get 'about', to: 'static_pages#about'
+  get 'careers', to: 'static_pages#careers'
   resources :courses, only: [:index, :show] do
     resources :enrollments, only: :create
   end
